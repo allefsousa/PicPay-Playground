ff6fcf4f383237608042e60f547898a8d4f9360d
Allef Sousa <allefsousa_1@hotmail.com>
2020-08-01 01:55:38 -0300

    Criando Modelo de cartão

---
+++ app/src/main/java/com/developer/allef/picpay/data/model/CredCard.kt
@@ -0,0 +1,13 @@
+package com.developer.allef.picpay.data.model
+
+/**
+ * @author allef.santos on 01/08/20
+ */
+data class CredCard(
+    val cardNumber: String,
+    val name: String,
+    val cvv: Int,
+    val value: Double,
+    val expiryDate: String,
+    val destinationUserId: Int
+)
