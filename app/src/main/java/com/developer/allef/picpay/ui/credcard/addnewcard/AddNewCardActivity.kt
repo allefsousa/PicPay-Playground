42668154128c849287f41c740ceda803a574a31b
Allef Sousa <allefsousa_1@hotmail.com>
2020-08-02 12:13:18 -0300

    Validadores de Cpf e cnpj

--- app/src/main/java/com/developer/allef/picpay/ui/credcard/addnewcard/AddNewCardActivity.kt
+++ app/src/main/java/com/developer/allef/picpay/ui/credcard/addnewcard/AddNewCardActivity.kt
@@ -2,6 +2,9 @@ package com.developer.allef.picpay.ui.credcard.addnewcard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
 import com.developer.allef.picpay.databinding.ActivityAddNewCardBinding
 import com.developer.allef.picpay.databinding.ActivityCredCardBinding
 
@@ -17,5 +20,19 @@ class AddNewCardActivity : AppCompatActivity() {
         binding.toolbar.componentToolbar.setNavigationOnClickListener {
             finish()
         }
+        binding.edCredNumber.addTextChangedListener(object :TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+        })
     }
 }
